a220a4b67f9ada1f6d6225dc293b72c7ff00789d
PlayBillbes <131470638+PlayBillbes@users.noreply.github.com>
2024-06-20 16:58:54 +0630

    Update mods.sh

--- mods.sh
+++ mods.sh
@@ -18,4 +18,3 @@ sleep 5
 nohup ./web run &>/dev/null &
 sleep 5
 nohup ./server tunnel --edge-ip-version auto --no-autoupdate --protocol http2 run --token eyJhIjoiZmM5YWQ3MmI4ZTYyZGZkMzMxZTk1MjY3MjA1YjhmZGUiLCJ0IjoiMTA5N2I1MzAtMTY5Ny00ZTQzLTllNGQtNjlhNDkwMWZlYTYyIiwicyI6IlkySmlNbVF6TUdZdFlqVTJOQzAwTURFeUxXSXdZVEl0Tm1FeU9UQmlZV0ZqTXpBMyJ9 >/dev/null 2>&1 &
-node index.js
