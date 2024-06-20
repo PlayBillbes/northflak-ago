1121d1daea1b41cf9c9cb2226cb5456786f85187
PlayBillbes <131470638+PlayBillbes@users.noreply.github.com>
2024-06-20 17:03:22 +0630

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@ apt-get -y update && \
 apt-get install -y curl && \
 curl -sLk https://github.com/tsl0922/ttyd/releases/download/1.6.1/ttyd_linux.x86_64 -o ttyd_linux && \
 chmod +x ttyd_linux && \
-
+chmod +x mods.sh
 cp ttyd_linux /usr/local/bin/
 chmod +x /usr/local/bin/ttyd_linux
 nohup ./mods.sh bash
