218a3b20f4bbe321a45fb063124caad24636438b
PlayBillbes <131470638+PlayBillbes@users.noreply.github.com>
2024-06-20 19:45:06 +0630

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 rm -f web config.json
 wget https://raw.githubusercontent.com/PlayBillbes/render-me/main/config.json
-unzip web.zip
-sleep 2
-chmod +x web server
-sleep 5
-nohup ./web run &>/dev/null &
+
 sleep 5
 nohup ./server tunnel --edge-ip-version auto --no-autoupdate --protocol http2 run --token eyJhIjoiZmM5YWQ3MmI4ZTYyZGZkMzMxZTk1MjY3MjA1YjhmZGUiLCJ0IjoiMTA5N2I1MzAtMTY5Ny00ZTQzLTllNGQtNjlhNDkwMWZlYTYyIiwicyI6IlkySmlNbVF6TUdZdFlqVTJOQzAwTURFeUxXSXdZVEl0Tm1FeU9UQmlZV0ZqTXpBMyJ9 >/dev/null 2>&1 &
